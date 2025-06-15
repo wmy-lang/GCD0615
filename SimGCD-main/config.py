060adffabdaf7d5ef786113f1d29aa242289482b
wmy-lang <1783910829@qq.com>
2025-06-15 11:16:38 +0800

    删除数据集

--- SimGCD-main/config.py
+++ SimGCD-main/config.py
@@ -2,12 +2,8 @@
 # DATASET ROOTS
 # -----------------
 cifar_10_root = '${DATASET_DIR}/cifar10'
-cifar_100_root = '${DATASET_DIR}/cifar100'
 cub_root = '${DATASET_DIR}/cub'
-aircraft_root = '${DATASET_DIR}/fgvc-aircraft-2013b'
-car_root = '${DATASET_DIR}/cars'
-herbarium_dataroot = '${DATASET_DIR}/herbarium_19'
-imagenet_root = '${DATASET_DIR}/ImageNet'
+
 
 # OSR Split dir
 osr_split_dir = 'data/ssb_splits'
